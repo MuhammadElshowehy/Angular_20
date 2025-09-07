232b30cfa0a88f918d4ad3bf9224e02011f3a9c1
MuhammadElshowehy <muhammadgamalelshowehy@gmail.com>
2025-09-07 23:55:03 +0300

    create component dynamically.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,15 @@
-import { Component } from '@angular/core';
+import {
+  AfterContentChecked,
+  Component,
+  computed,
+  ElementRef,
+  inject,
+  OnInit,
+  Signal,
+  TemplateRef,
+  viewChild,
+  ViewContainerRef,
+} from '@angular/core';
 import { StartWithSignalsComponent } from './start-with-signals/start-with-signals.component';
 import { ParentComponent } from './input-and-output/components/parent/parent.component';
 import { FormsModule } from '@angular/forms';
@@ -7,14 +18,22 @@ import { Option1 } from './render-comps-programatically/option-1/option-1';
 import { NgComponentOutlet } from '@angular/common';
 @Component({
   selector: 'app-root',
-  imports: [ParentComponent, FormsModule, NgComponentOutlet],
+  imports: [ParentComponent, FormsModule, NgComponentOutlet, Option2],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {
+export class App implements OnInit {
   selectedCompName: 'one' | 'two' = 'one';
-
   get selectedComp() {
     return this.selectedCompName === 'one' ? Option1 : Option2;
+  }
+
+  vcr = inject(ViewContainerRef);
+  option2: Signal<TemplateRef<any> | undefined> =
+    viewChild<TemplateRef<any>>('option2');
+
+  ngOnInit() {
+    // this.vcr.createComponent(Option1);
+    this.vcr.createEmbeddedView(this.option2()!);
   }
 }
